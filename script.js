b7534edf9f52a56001631558add15475f8fd85f2
AdilTehranli <adiljt@code.edu.az>
2023-06-07 22:40:45 +0400

    Updated

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let list = [];
 let listul = document.getElementById("list");
 function todo() {
   let inp = document.getElementById("inp");
-  if(inp.value.length===0){
+
+   if(list.includes(inp.value)){
+    alert('This word is available')
+
+  }
+
+  else if(inp.value.length===0){
     alert('empty')
   }else{
     list.push(inp.value);
@@ -25,12 +31,13 @@ function Display(){
     }
     listul.innerHTML = data;
 }
-// let nums = [1, 2, 3, 4];
-// let sum=0;
-// for(let i =0;i<9;i++){
-//   let num= nums.slice(nums.length-3);
-//   sum=num.reduce((a,b)=>a+b);
-//   nums.push(sum)
 
-// }
-// console.log(nums)
+let nums = [1, 2, 3, 4];
+let sum=0;
+for(let i =0;i<9;i++){
+  let num= nums.slice(nums.length-3);
+  sum=num.reduce((total,amount)=>total+amount);
+  nums.push(sum)
+
+}
+console.log(nums)
